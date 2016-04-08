b0dcb25e97dfdbeaa8c94b76567f45611ba67127
rocketII <vangandr@gmail.com>
2016-04-08 12:52:47 +0200

    Gift class done and tested.

--- Gift.cpp
+++ Gift.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "Gift.h"
+#include <sstream>
+int Gift::getPrice() const
+{
+    return this->price;
+}
+
+string Gift::getRecipient() const
+{
+    return this->recipient;
+}
+
+string Gift::getWhat() const
+{
+    return this->what;
+}
+
+void Gift::setPrice(int price)
+{
+    this->price=price;
+}
+
+void Gift::setRecipient(string recipient)
+{
+    this->recipient= recipient;
+}
+
+void Gift::setWhat(string what)
+{
+    this->what= what;
+}
+string Gift::toString() const
+{
+    stringstream rr;
+    rr <<"\npresent: "<< this->getWhat() <<"\nprice: "<< this->getPrice() << "\nrecipient: "<<this->getRecipient();
+    return rr.str() ;
+}
+
+bool Gift::getBought(void) const
+{
+    return this->bought;
+}
+
+void Gift::setBought(bool bought)
+{
+    this->bought=bought;
+}
--- Gift.h
+++ Gift.h
@@ -1,14 +1,31 @@
 //
 // Created by root on 2016-04-08.
+// This class is tested and working.
 //
 
 #ifndef ASSIGNMENTA_GIFT_H
 #define ASSIGNMENTA_GIFT_H
 
-
-class Gift {
+#include <string>
+using namespace std;
+class Gift
+{
 private:
+    string what="empty";
+    string recipient="N/A";
+    int price=-1;
+    bool bought=false;
+
 public:
+    string getWhat(void)const;
+    string getRecipient(void)const;
+    int getPrice(void)const;
+    bool getBought(void)const;
+    void setWhat(string);
+    void setRecipient(string);
+    void setPrice(int);
+    void setBought(bool);
+    string toString(void)const;
 };
 
 
